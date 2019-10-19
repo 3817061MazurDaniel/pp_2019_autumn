ff450fd30d4620e00a6696c8fcca46d7d1a2fe42
Mazur Daniil <mazurdany@yandex.ru>
2019-10-20 00:55:24 +0300

    Done

---
+++ modules/task_1/mazur_d_monte_carlo/monte_carlo.h
@@ -0,0 +1,14 @@
+#ifndef MODULES_TASK_1_MAZUR_D_INT_MONTE_CARLO_H_
+#define MODULES_TASK_1_MAZUR_D_INT_MONTE_CARLO_H_
+
+#include <iostream>
+#include <mpi.h>
+#include <math.h>
+using namespace std;
+
+
+double getIntegral(int nPoints,  double(*func)(double), double x1, double x2);
+
+
+
+#endif  // MODULES_TASK_1_MAZUR_D_INT_MONTE_CARLO_H_
